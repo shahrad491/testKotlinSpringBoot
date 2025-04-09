7f88a0c295decc330113ccd182e9822cf2c28cf7
Shahrad <shahrad491@gmail.com>
2025-04-09 03:34:25 +0330

    made author&books data classes and connected them to postgresql and made Entities

--- src/main/kotlin/com/testbook/BookStore/domain/Author.kt
+++ src/main/kotlin/com/testbook/BookStore/domain/Author.kt
@@ -1,6 +1,22 @@
 package com.testbook.BookStore.domain
 
-class Author(val id: Long?, val name: String, val age: Int, val description: String, val image: String){
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "authors")
+data class Author(
+    @Id
+    @Column(name = "author_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_id_seq")
+    val id: Long?,
+    @Column(name = "author_name")
+    val name: String,
+    @Column(name = "author_age")
+    val age: Int,
+    @Column(name = "author_description")
+    val description: String,
+    @Column(name = "author_image")
+    val image: String){
 
 
 }
--- src/main/kotlin/com/testbook/BookStore/domain/Book.kt
+++ src/main/kotlin/com/testbook/BookStore/domain/Book.kt
@@ -1,4 +1,22 @@
 package com.testbook.BookStore.domain
 
-class Book(val isbn: String, val title: String,val description:String, val author: Author) {
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "books")
+data class Book(
+    @Id
+    @Column(name = "book_id")
+    val isbn: String,
+    @Column(name = "book_title")
+    val title: String,
+    @Column(name = "book_description")
+    val description:String,
+
+    @Column(name = "book_image")
+    val image: String,
+
+    @ManyToOne(cascade = [(CascadeType.DETACH)])
+    @JoinColumn(name = "author_id")
+    val author: Author) {
 }
